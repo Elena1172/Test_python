2cb740ce7b99ac35526e1f8e8167baf910fcd6b9
Elena <zagre8elena@yandex.ru>
2023-01-07 11:57:16 +0300

    change file

--- ff.py
+++ ff.py
@@ -1,1 +1,9 @@
-print ('Hello')
+def gcd(a,b):
+    if b == 0:
+        return a
+    return gcd(b,a%b)
+print (gcd(17,51))
+def gcd1(a,b):
+    return a if b == 0 else gcd1(b,a%b)
+print (gcd(51,5))
+print (gcd1(5,51))
